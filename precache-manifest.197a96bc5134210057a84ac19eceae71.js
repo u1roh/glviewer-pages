3b6c3c5d9cdf7d27b29704a445877f60410125f9
Yuichiro Terada <yuichiro_terada@caddi.jp>
2020-07-03 23:56:32 +0900

    first version

---
+++ precache-manifest.197a96bc5134210057a84ac19eceae71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ccc03ce0e5535d77d6b0c54a18daa7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cc3a2900c6f5570b52f",
+    "url": "/static/css/main.a59b5c63.chunk.css"
+  },
+  {
+    "revision": "cf879b14836326c1929a",
+    "url": "/static/js/2.e1bf60cd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.e1bf60cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cc3a2900c6f5570b52f",
+    "url": "/static/js/main.43824283.chunk.js"
+  },
+  {
+    "revision": "7b0760817e8205f380af",
+    "url": "/static/js/runtime-main.a98feb21.js"
+  }
+]);
